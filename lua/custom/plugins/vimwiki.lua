b56e9ad3c5fa9d71080d1e8d0b9ae1837d640c40
Geert Verbist <bist@bist.be>
2023-11-06 19:45:17 +0100

    Added filetree and vimwiki

---
+++ lua/custom/plugins/vimwiki.lua
@@ -0,0 +1,20 @@
+return {
+    "vimwiki/vimwiki",
+    event = "BufEnter *.md",
+    keys = { "<leader>ww", "<leader>wt" },
+    init = function()
+        vim.g.vimwiki_folding = ""
+        vim.g.vimwiki_list = {
+            {
+                path = "~/vimwiki",
+                syntax = "markdown",
+                ext = ".md",
+            },
+        }
+        vim.g.vimwiki_ext2syntax = {
+            [".md"] = "markdown",
+            [".markdown"] = "markdown",
+            [".mdown"] = "markdown",
+        }
+    end,
+}
